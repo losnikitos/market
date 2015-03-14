b1e4e1882aa494dfda3bb227ca9ad4340d901281
Nikita Popov <losnikitos@yandex-team.ru>
2015-03-15 00:46:11 +0200

    Parse xls

--- static/upload/upload.js
+++ static/upload/upload.js
@@ -4,6 +4,17 @@ angular
         $scope.uploader = new FileUploader();
 
         $scope.uploader.onAfterAddingFile = function (fileItem) {
-            console.info('onAfterAddingFile', fileItem);
+            var reader = new FileReader();
+            reader.onload = function(e) {
+                var data = e.target.result;
+                var workbook = XLS.read(data, {type: 'binary'});
+
+                $scope.goods = workbook.Strings.map(function(str) {return str.t});
+
+                //var Sheet1A1 = workbook.Sheets[workbook.SheetNames[0]]['A1'].v;
+
+                console.log($scope.goods);
+            };
+            reader.readAsBinaryString(fileItem._file);
         };
     });
